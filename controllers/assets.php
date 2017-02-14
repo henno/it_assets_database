339e350c256ee3eea7b832f9735d6b48b7a24078
Henno Täht <henno.taht@gmail.com>
2017-02-14 11:27:54 +0200

    Esialgne commit

---
+++ controllers/assets.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: henno
+ * Date: 14/02/17
+ * Time: 11:09
+ */
+
+namespace Halo;
+
+
+class assets extends Controller
+{
+
+    function index(){
+
+    }
+
+}
